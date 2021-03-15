9aa366b897e4817114fdef3a33b3ae8c9417d433
Christopher Bauer <30803717+CBauer99@users.noreply.github.com>
2021-03-15 00:08:39 -0400

    Updated comments in con.php

--- connection.php
+++ connection.php
@@ -1,8 +1,10 @@
 <?php
 
+// Need to figure out how to host non locally
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
+// DB name in phpMyAdmin
 $dbname = "baue3081";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
